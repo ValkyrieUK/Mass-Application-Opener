cde71e7373c60a6a6f8e67be431d981b5f7ded65
Bradley Allen <valkyrieuk@Labs-MacBook-Air.local>
2014-01-02 15:31:52 +0000

    Refactored and added checks to see if array is empty

--- app.rb
+++ app.rb
@@ -1,33 +1,51 @@
 require 'etc'
 require 'active_support/core_ext/integer/inflections'
-@today = Time.now
-@day = @today.strftime("%A")
-@month =  Date::MONTHNAMES[Date.today.month]
-@user = Etc.getlogin
-
-@app = ['Skype', 'Microsoft\ Lync', 'Google\ Chrome', 'Microsoft\ Office\ 2011/Microsoft\ Outlook']
-#Add the Disered programs to the array above as the are named in your
-#Applications folder. Note if the app name contans a space name like so
-# 'Folder\ with\ a\ space\'
-
-
-p "Welcome #{@user}, Today is #{@day} the #{@today.day.ordinalize} of #{@month}, #{@today.year}"
-sleep 1
-system ("clear")
-p "The following programs will be opened"
-
-
-@app.each {|x| p x }
-p "Do you wish to contine Y/N ?"
-
-
-sleep 2
-@yn = gets.chomp
-
-case @yn
-when 'y', 'yes', 'yeah', 'yea'
-  @app.each do |i|
-   system("open ~/../../Applications/#{i}.app")
+require 'active_support'
+  def greeting
+    today = Time.now
+    day = today.strftime("%A")
+    month =  Date::MONTHNAMES[Date.today.month]
+    user = Etc.getlogin
+    puts "Welcome #{user}, Today is #{day} the #{today.day.ordinalize} of #{month}, #{today.year}"
+    sleep 2
+    system ("clear")
   end
 
-end
+
+  def applications
+    @app = []
+    puts "The following programs will be opened"
+    puts @app.each {|x|}
+    sleep 1
+    p "Do you wish to contine Y/N ?"
+    #Add the Disered programs to the array above as the are named in your
+    #Applications folder. Note if the app name contans a space name like so
+    # 'Folder\ with\ a\ space\'
+  end
+
+  def open_apps
+    if @app == []
+      puts "********" 
+      puts "Please add applications to the @app array".upcase
+      puts "********"
+    else
+      @yn = gets.chomp
+      case @yn
+      when 'y', 'Y', 'yes', 'YES', 'yeah', 'yea'
+         @app.each do |i|
+         system("open ~/../../Applications/#{i}.app")
+        end
+      when 'no', 'n', 'NO', 'No'
+      nil
+    else
+      puts 'Please enter Yes or No'
+      open_apps
+      end
+    end
+  end
+
+greeting
+applications
+open_apps
+
+
