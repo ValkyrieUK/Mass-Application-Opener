135900be2aadbbdfc0a8223fa706a5da1cde512e
Bradley Allen <valkyrieuk@Labs-MacBook-Air.local>
2014-01-02 15:47:51 +0000

    added the ability to add apps to the array

--- app.rb
+++ app.rb
@@ -1,6 +1,5 @@
 require 'etc'
 require 'active_support/core_ext/integer/inflections'
-require 'active_support'
   def greeting
     today = Time.now
     day = today.strftime("%A")
@@ -17,7 +16,6 @@ require 'active_support'
     puts "The following programs will be opened"
     puts @app.each {|x|}
     sleep 1
-    p "Do you wish to contine Y/N ?"
     #Add the Disered programs to the array above as the are named in your
     #Applications folder. Note if the app name contans a space name like so
     # 'Folder\ with\ a\ space\'
@@ -28,19 +26,39 @@ require 'active_support'
       puts "********" 
       puts "Please add applications to the @app array".upcase
       puts "********"
+      sleep 2
+      add_apps
     else
+    p "Do you wish to contine Y/N ?"
       @yn = gets.chomp
       case @yn
-      when 'y', 'Y', 'yes', 'YES', 'yeah', 'yea'
-         @app.each do |i|
-         system("open ~/../../Applications/#{i}.app")
-        end
-      when 'no', 'n', 'NO', 'No'
-      nil
-    else
-      puts 'Please enter Yes or No'
-      open_apps
+        when 'y', 'Y', 'yes', 'YES', 'yeah', 'yea'
+           @app.each do |i|
+           system("open ~/../../Applications/#{i}.app")
+          end
+        when 'no', 'n', 'NO', 'No'
+        nil
+      else
+        puts 'Please enter Yes or No'
+        open_apps
       end
+    end
+  end
+
+
+  def add_apps
+    @new_app = gets.chomp
+    @app << @new_app
+    puts "Current apps in array #{@app}"
+    sleep 1
+    puts "Would you like to add any more apps ?"
+    @yn = gets.chomp
+    case @yn 
+      when 'y', 'Y', 'yes', 'YES', 'yeah', 'yea'
+        puts "Whats is the name of the other app you would like to open"
+        add_apps
+      when 'no', 'n', 'NO', 'No'
+        open_apps
     end
   end
 
